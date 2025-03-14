3b83cf1dda58f7dab27b0d27a8f822dc7ba38077
yogeshcoded <yogeshbyjava@gmail.com>
2025-03-14 20:59:31 +0530

    tsts

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -3,6 +3,6 @@ package com.nt;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!jkjjjjjjjjjjj");
     }
 }
