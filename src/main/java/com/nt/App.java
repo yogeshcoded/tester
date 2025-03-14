cbfdfc9dacbfb0b6b3f6b38b3384bd331e483588
yogeshcoded <yogeshbyjava@gmail.com>
2025-03-14 20:55:42 +0530

    teter fixed issue

--- src/main/java/com/nt/App.java
+++ src/main/java/com/nt/App.java
@@ -3,6 +3,6 @@ package com.nt;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World! K");
+        System.out.println("Hello World!");
     }
 }
